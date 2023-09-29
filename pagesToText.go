7bd873ed6390074a416e948facea47733752bf23
pilinux <mahir.hasan@m.pilinux.me>
2023-09-29 17:06:28 +0200

    convert pages to text - todo

---
+++ pagesToText.go
@@ -0,0 +1,20 @@
+package totext
+
+import (
+	"os"
+
+	"code.sajari.com/docconv"
+)
+
+// ConvertPagesToText receives pages filepath as an argument and returns its text content and metadata
+func ConvertPagesToText(filepath string) (content string, metadata map[string]string, err error) {
+	// Get the pages file
+	pagesFile, err := os.Open(filepath)
+	if err != nil {
+		return "", nil, err
+	}
+
+	// Convert pages to text
+	content, metadata, err = docconv.ConvertPages(pagesFile)
+	return
+}
