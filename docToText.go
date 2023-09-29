9d8ed563a08eb4721e72675b30527447b62a8e92
pilinux <mahir.hasan@m.pilinux.me>
2023-09-29 12:23:35 +0200

    ConvertDocToText - added OS dependency info

--- docToText.go
+++ docToText.go
@@ -8,6 +8,12 @@ import (
 
 // ConvertDocToText receives MS word doc filepath as an argument
 // and returns its text content and metadata
+//
+// Dependencies:
+//
+// Debian/Ubuntu: sudo apt install wv
+//
+// MacOS: brew install wv
 func ConvertDocToText(filepath string) (content string, metadata map[string]string, err error) {
 	// Get the doc file
 	docFile, err := os.Open(filepath)
