29a810c0c3d62f27138973aa3b71032539bb6950
pilinux <mahir.hasan@m.pilinux.me>
2023-09-29 07:11:56 +0200

    get version info of cli

--- version.go
+++ version.go
@@ -2,6 +2,12 @@
 // to extract text from different file types.
 package totext
 
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
 // Build information
 var (
 	AppVersion string
@@ -9,3 +15,19 @@ var (
 	CommitHash string
 	Author     string
 )
+
+// Version returns the version number, build date and commit hash
+func Version(appName string) *cobra.Command {
+	var versionCmd = &cobra.Command{
+		Use:   "version",
+		Short: "Print the version number, build date and commit hash",
+		Run: func(cmd *cobra.Command, args []string) {
+			fmt.Println(appName, "version:", AppVersion)
+			fmt.Println("Build date:", BuildDate)
+			fmt.Println("Commit hash:", CommitHash)
+			fmt.Println("Author:", Author)
+		},
+	}
+
+	return versionCmd
+}
